0e9cd7a5d57344cf1802aef0a579639eeee82952
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-06 14:02:29 +0200

    Update version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # This is the basic information about your project. Modify all this
     # information before releasing code publicly.
     name='xbob.example.faceverify',
-    version='0.5.0',
+    version='0.5.1a0',
     description='Example for using Bob to create face verification systems',
     url='http://pypi.python.org/pypi/xbob.example.faceverify',
     license='GPLv3',
